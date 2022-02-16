4f25cbed4f92225571a84fa8b3ae6d6bfff88873
Sergei Golubchik <vuvova@gmail.com>
2022-02-16 21:46:44 +0100

    best avg starting word in wordle

---
+++ best_1st-avg.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+#include "w.h"
+
+#define GUESSES (sizeof(guesses)/sizeof(guesses[0]))
+#define WORDS (sizeof(words)/sizeof(words[0]))
+
+int cmps[WORDS];
+
+int compare(const char *g, const char *w) {
+#define one(G,W1,W2,W3,W4) (\
+  (g[G] == w[G] ? 1 : \
+   g[G] == w[W1] || g[G] == w[W2] || g[G] == w[W3] || g[G] == w[W4] ? 2 : 3) \
+   << (G*2))
+
+  return one(0,1,2,3,4) |
+         one(1,0,2,3,4) |
+         one(2,0,1,3,4) |
+         one(3,0,1,2,4) |
+         one(4,0,1,2,3);
+}
+
+int main() {
+  int mincnt=WORDS*WORDS, ming=0;
+  printf("%lu %lu\n", GUESSES, WORDS);
+  for (int g=0; g < GUESSES; g++) {
+    int cnt=0;
+    for (int w=0; w < WORDS; w++) {
+      cmps[w]=compare(guesses[g], words[w]);
+    }
+    for (int w=0; w < WORDS; w++) {
+      for (int ww=0; ww < WORDS; ww++) {
+        cnt+= cmps[w] == cmps[ww];
+      }
+    }
+    if (cnt >= mincnt) continue;
+    mincnt=cnt;
+    ming=g;
+  }
+  printf("%s %f\n", guesses[ming], ((double)mincnt)/WORDS);
+}
+/* roate 60.132092 */
